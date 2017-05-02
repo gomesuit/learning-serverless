2965b85b47681fc6b3546012f7411765660a809a
gomesuit <gomesuit@gmail.com>
2017-05-02 14:13:17 +0900

    save

--- provision.sh
+++ provision.sh
@@ -15,6 +15,20 @@ curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 sudo python get-pip.py
 sudo pip install awscli
 
-
 # aws configure
 
+# install common package
+yum install -y vim
+
+
+
+#mkdir hello-serverless
+#cd hello-serverless
+#sls create --template aws-nodejs --name hello
+# vim serverless.yml
+#sls deploy -v
+#sls invoke -f hello
+# vim serverless.yml
+#sls deploy -v
+# curl -X GET https://xxxxxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/dev/hello
+#sls remove -v
