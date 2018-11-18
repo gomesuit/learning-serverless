997c21252f1b31e613012a3253dcf72c1df793ff
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-11-18 11:56:27 +0900

    save

--- samples/python/aws-notice/handler.py
+++ samples/python/aws-notice/handler.py
@@ -1,24 +1,29 @@
+import boto3
 import json
+import logging
+import os
 
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+SLACK_HOOK_URL = os.environ['SLACK_HOOK_URL']
+SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'text': "test"
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    req = Request(SLACK_HOOK_URL, json.dumps(slack_message).encode('utf-8'))
+    try:
+        response = urlopen(req)
+        response.read()
+        logger.info("Message posted to %s", slack_message['channel'])
+    except HTTPError as e:
+        logger.error("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        logger.error("Server connection failed: %s", e.reason)
