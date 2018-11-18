b3f469a658eeddb4eb71efb8729c06c20d5d7e34
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-11-18 12:07:39 +0900

    save

--- samples/python/aws-notice/handler.py
+++ samples/python/aws-notice/handler.py
@@ -15,7 +15,23 @@ logger.setLevel(logging.INFO)
 def hello(event, context):
     slack_message = {
         'channel': SLACK_CHANNEL,
-        'text': "test"
+        'text': "hello"
+    }
+
+    req = Request(SLACK_HOOK_URL, json.dumps(slack_message).encode('utf-8'))
+    try:
+        response = urlopen(req)
+        response.read()
+        logger.info("Message posted to %s", slack_message['channel'])
+    except HTTPError as e:
+        logger.error("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        logger.error("Server connection failed: %s", e.reason)
+
+def login(event, context):
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'text': "login"
     }
 
     req = Request(SLACK_HOOK_URL, json.dumps(slack_message).encode('utf-8'))
