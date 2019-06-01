035a34797496bab379979a2ffd5175441c002bf7
gomesuit <9404411+gomesuit@users.noreply.github.com>
2019-06-01 19:20:09 +0900

    save

--- samples/ruby/domain/handler.rb
+++ samples/ruby/domain/handler.rb
@@ -3,6 +3,6 @@ require 'json'
 def hello(event:, context:)
   {
     statusCode: 200,
-    body: JSON.generate('Go Serverless v1.0! Your function executed successfully!')
+    body: JSON.generate(event)
   }
 end
